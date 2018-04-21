f622e4ce2e8c3b2a8db1aed735b53164db99a2e4
Remaldeep <technoremal@gmail.com>
2018-04-20 19:28:11 -0700

    Fixed CRF code.
    Added outputs and  reversed the probabilities.

--- CasenetCRF/beliefpropogation.py
+++ CasenetCRF/beliefpropogation.py
@@ -1,34 +1,85 @@
 import numpy as np
-def beliefpropogation(G, unary_cost, vnodes, fnodes, messages):
+import scipy.io
+from PIL import Image
+import pickle
+import json
+
+def beliefpropogation(G, beliefsG, beliefs_sum, unary_cost, v_indexes, vnodes, fnodes, messages, removed_vnodes, features, label_colors, defaultG):
 
     l_values = 19
     iterations = 20
     for iter in range(iterations):
-
+        with open('./Outputs/' + str(iter) + '.pickle', 'wb') as handle:
+            pickle.dump(messages, handle, protocol=0)
+        # if iter >= 17:
+        #     with open('./Outputs/' + str(iter) + '.json', 'w') as handle:
+        #         handle.write(str(messages))
+        print('Iteration: ' + str(iter))
+        beliefsL = np.zeros(beliefsG.shape, 'int')
         #Messages from factor node to variable node.
-        for id in range(vnodes, vnodes+fnodes):
-            nbrs = G.neighbors(id)
-            nbr1 = 0
-            nbr2 = 0
+
+        for f_id in range(vnodes, vnodes+fnodes):
+            if f_id%100000 == 0:
+                print('s: ' + str(vnodes) + ' c: ' + str(f_id) + ' e: ' + str(vnodes+fnodes))
+            nbrs = G.neighbors(f_id)
+            v_id1 = 0
+            v_id2 = 0
             count = 0
             for nbr in nbrs:
                 if count == 0:
-                    nbr1 = nbr
+                    v_id1 = nbr
                 else:
-                    nbr2 = nbr
-            findMinimum(l_values, nbr1, id, messages, 1)
+                    v_id2 = nbr
+                count += 1
+            messages[str(f_id) + '-' + str(v_id1)] = findMinimum(l_values, v_id2, f_id, messages, 10)
+            messages[str(f_id) + '-' + str(v_id2)] = findMinimum(l_values, v_id1, f_id, messages, 10)
 
-        # Updating beliefs.
-        for id in range(vnodes):
-            nbrs = G.neighbors(id)
-            for nbr in nbrs:
+        #Updating beliefs.
+        for v_id in v_indexes:
+            if v_id in removed_vnodes:
+                continue
+            dummy = np.zeros((19, 1), 'float')
+            nbrs = G.neighbors(v_id)
+            for f_id in nbrs:
+                dummy = dummy + messages[str(f_id) + '-' + str(v_id)]
+            #Adding the unary cost now.
+            dummy = dummy + unary_cost[v_id]
+            beliefs_sum[v_id] = dummy
 
+        # if iter >= 17:
+        #     with open('./Outputs/' + str(iter) + '_beliefs_sum.json', 'w') as handle:
+        #         handle.write(str(beliefs_sum))
+
+        #Taking prediction.
+        for v_id in v_indexes:
+            if v_id in removed_vnodes:
+                continue
+            v_array = beliefs_sum[v_id]
+            idx = np.argmin(v_array)
+            beliefsL[v_id] = idx
+
+        #Checkif label has changed
+        if np.array_equal(beliefsG,beliefsL):
+            print('Labels are not changing.')
+            break
+        else:
+            beliefsG = beliefsL
+
+        visualize_data(beliefsG, features, iter, label_colors, defaultG)
+        #Updating the messages from variable node to factor node.
+        for v_id in v_indexes:
+            if v_id in removed_vnodes:
+                continue
+            nbrs = G.neighbors(v_id)
+            for f_id in nbrs:
+                val = beliefs_sum[v_id] - messages[str(f_id) + '-' + str(v_id)]
+                messages[str(v_id) + '-' + str(f_id)] = val
 
     print('yes')
 
-def findMinimum(l_values, nbr, f_node, messages, lamda):
-    finalV = np.zeros(l_values, 1)
-    m_cost = messages[str(f_node) + '-' + str(nbr)]
+def findMinimum(l_values, v_node, f_node, messages, lamda):
+    finalV = np.zeros((l_values, 1), 'float')
+    m_cost = messages[str(v_node) + '-' + str(f_node)]
     for i in range(l_values):
         # Taking the minimum cost.
         costM = lamda
@@ -43,25 +94,41 @@ def findMinimum(l_values, nbr, f_node, messages, lamda):
             costV = costV + m_cost[j]
             if minV > costV:
                 minV = costV
-        finalV[i, 1] = minV
+        finalV[i, 0] = minV
     return finalV
 
-def findMinimum2(l_values, nbrL, nbrR, f_node, messages, lamda):
-    finalV = np.zeros(l_values, 1)
-    m_cost = messages[str(f_node) + '-' + str(nbrL)]
-    for i in range(l_values):
-        # Taking the minimum cost.
-        costM = lamda
-        if i == 0:
-            costM = 0
-        minV = costM + m_cost[0]
 
-        for j in range(l_values):
-            costV = lamda
-            if i == j:
-                costV = 0
-            costV = costV + m_cost[j]
-            if minV > costV:
-                minV = costV
-        finalV[i, 1] = minV
-    return finalV
+def visualize_data(beliefs, features, iter, label_colors, defaultG):
+    defaultG = defaultG.reshape(defaultG.shape[0], -1)
+    imWidth = 640
+    imHeight = 360
+    image = Image.new('RGB', (imWidth, imHeight))
+    imagePix = image.load()
+
+    for i in range(imHeight):
+        for j in range(imWidth):
+            imagePix[j, i] = (0, 0, 0)
+
+    vShape = features.shape
+    rows = vShape[0]
+
+    # Pixel value with labels.
+    for j in range(rows):
+        row = features[j]
+        y = int(round(row[0] * imHeight))
+        x = int(round(row[1] * imWidth))
+        idx = beliefs[j]
+        val = label_colors[idx[0]]
+        imagePix[x, y] = val
+        # for i in range(no_of_features):
+        # print(int(row[0]*imWidth), int(row[1]*imHeight))
+    #image.show()
+    image.save('./Outputs/' + str(iter) + '.jpg')
+    find_cost(beliefs, defaultG)
+
+def find_cost(beliefs, defaultG):
+    count = 0
+    for j in range(beliefs.shape[0]):
+        if beliefs[j] != defaultG[j]:
+            count += 1
+    print('Cost= ' + str(count))
--- CasenetCRF/main.py
+++ CasenetCRF/main.py
@@ -1,23 +1,230 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import scipy.io
+import scipy.io as sio
 from PIL import Image
 import numpy as np
 from beliefpropogation import beliefpropogation
 
 
 def main(w, h):
-    # 360x640
-    imWidth = 640
-    imHeight = 360
+
+    label_colors={}
+    label_colors[0] = (225, 0, 0)
+    label_colors[1] = (0, 225, 0)
+    label_colors[2] = (0, 0, 225)
+    label_colors[3] = (255, 255, 0)
+    label_colors[4] = (255, 0, 255)
+    label_colors[5] = (0, 255, 255)
+    label_colors[6] = (210, 5, 72)
+    label_colors[7] = (19, 247, 11)
+    label_colors[8] = (123, 119, 36)
+    label_colors[9] = (159, 85, 192)
+    label_colors[10] = (255, 255, 255)
+    label_colors[11] = (138, 98, 200)
+    label_colors[12] = (199, 188, 112)
+    label_colors[13] = (196, 95, 124)
+    label_colors[14] = (187, 136, 121)
+    label_colors[15] = (28, 191, 140)
+    label_colors[16] = (87, 126, 40)
+    label_colors[17] = (83, 13, 83)
+    label_colors[18] = (207, 147, 86)
+    label_colors[19] = (48, 85, 62)
 
     G = nx.Graph()
-    createGraphFromImage(G, 640, 360)
-
+    createGraphFromImage(G, 640, 360, label_colors)
 
     #------------------------ OLD code
     #createGraph(G, imWidth, imHeight)
 
+    print('yes')
+
+def createGraphFromImage(G, w, h, label_colors):
+    mat = sio.loadmat('loop1_casenet_features/loop1_casenet_features/VDO_0018_3878.mat')    
+    features = mat['feat']
+    features = preprocess_features(features) #Preprocessing the features.
+    vShape = features.shape
+    no_of_features = vShape[1]
+    rows = vShape[0]
+
+    #Getting base labels
+    defaultG = visualize_image(label_colors, features)
+
+    # Adding value nodes and beliefs.
+    beliefs_sum = {}
+    beliefs = np.zeros((rows, 1), 'int')
+    for j in range(rows):
+        #row = features[j]
+        #G.add_node(j, bipartite=0)
+        beliefs_sum[j] = np.zeros((19, 1), dtype=object)
+
+    # Calculating unary costs.
+    unary_cost = []
+    for j in range(rows):
+        row = features[j]
+        cost = row[2:]
+        cost = cost.reshape(cost.shape[0], -1)
+        unary_cost.append(cost)
+
+    # Adding factor nodes.
+    imageP = np.zeros((h, w), 'int')
+    messages = {}
+    for i in range(rows):
+        row = features[i]
+        y = int(round(row[0] * h))
+        x = int(round(row[1] * w))
+        if i == 0:
+            imageP[y, x] = -1
+        else:
+            imageP[y, x] = i
+
+    v_indexes = []
+    #Adding variable nodes.
+    for i in range(h):
+        for j in range(w):
+            if imageP[i, j] != 0:
+                val = imageP[i, j]
+                if val == -1:
+                    val = 0
+                G.add_node(val, bipartite=0)
+                v_indexes.append(val)
+
+
+    removed_vnodes = []
+    offset = rows
+    count = 0
+    for i in range(h):
+        for j in range(w):
+            if imageP[i, j] != 0:
+                c_val = imageP[i, j]
+                if c_val == -1:
+                    c_val = 0
+                #Node to the right.
+                if j < w-1 and imageP[i, j + 1] != 0:
+                    id = offset + count
+                    G.add_node(id, bipartite=1)
+                    G.add_edge(id, c_val)
+                    G.add_edge(id, imageP[i, j+1])
+                    count += 1
+                    createMessages(messages, str(id), str(c_val))
+                    createMessages(messages, str(id), str(imageP[i, j + 1]))
+
+                #Node to the bottom.
+                if i < h-1 and imageP[i + 1, j] != 0:
+                    id = offset + count
+                    G.add_node(id, bipartite=1)
+                    G.add_edge(id, c_val)
+                    G.add_edge(id, imageP[i + 1, j])
+                    count += 1
+                    createMessages(messages, str(id), str(c_val))
+                    createMessages(messages, str(id), str(imageP[i + 1, j]))
+
+                # Node to the diagnol right.
+                if i < h - 1 and j < w - 1 and imageP[i + 1, j + 1] != 0:
+                    id = offset + count
+                    G.add_node(id, bipartite=1)
+                    G.add_edge(id, c_val)
+                    G.add_edge(id, imageP[i + 1, j + 1])
+                    count += 1
+                    createMessages(messages, str(id), str(c_val))
+                    createMessages(messages, str(id), str(imageP[i + 1, j + 1]))
+
+                # Noe to the diagnol left.
+                if i < h - 1 and j > 0 and imageP[i + 1, j - 1] != 0:
+                    id = offset + count
+                    G.add_node(id, bipartite=1)
+                    G.add_edge(id, c_val)
+                    G.add_edge(id, imageP[i + 1, j - 1])
+                    count += 1
+                    createMessages(messages, str(id), str(c_val))
+                    createMessages(messages, str(id), str(imageP[i + 1, j - 1]))
+
+                #Node to the right 2 pixels.
+                if j < w-2 and imageP[i, j + 2] != 0:
+                    id = offset + count
+                    G.add_node(id, bipartite=1)
+                    G.add_edge(id, c_val)
+                    G.add_edge(id, imageP[i, j + 2])
+                    count += 1
+                    createMessages(messages, str(id), str(c_val))
+                    createMessages(messages, str(id), str(imageP[i, j + 2]))
+
+                #Node to the bottom 2 pixels.
+                if i < h-2 and imageP[i + 2, j] != 0:
+                    id = offset + count
+                    G.add_node(id, bipartite=1)
+                    G.add_edge(id, c_val)
+                    G.add_edge(id, imageP[i + 2, j])
+                    count += 1
+                    createMessages(messages, str(id), str(c_val))
+                    createMessages(messages, str(id), str(imageP[i + 2, j]))
+
+                #Removing nodes without any factor nodes.
+                if checkNeighborhood(imageP, i, j, w, h) == 0:
+                    G.remove_node(c_val)
+                    removed_vnodes.append(c_val)
+                    v_indexes.remove(c_val)
+
+
+
+    nx.write_gexf(G, './graph.gexf')
+
+    # nbrs = G.neighbors(55)
+    # for nbr in nbrs:
+    #     print(nbr)
+    #     # nbrs1 = G.neighbors(nbr)
+    #     # for nbr1 in nbrs1:
+    #     #     print(nbr1)
+    #
+    # print('fuck')
+    nbrs = G.neighbors(112)
+    for nbr in nbrs:
+        s = str(nbr)
+        nbrs1 = G.neighbors(nbr)
+        for nbr1 in nbrs1:
+            s = s + '-' + str(nbr1)
+        print(s)
+
+    #rows will be greater than v_indexes.
+    print(v_indexes[len(v_indexes)-1])
+    beliefpropogation(G, beliefs, beliefs_sum, unary_cost, v_indexes, rows, count, messages, removed_vnodes, features, label_colors, defaultG)
+    print('done!')
+
+def createMessages(messages, str1, str2):
+    messages[str1 + '-' + str2] = np.zeros((19, 1), dtype=object)
+    messages[str2 + '-' + str1] = np.zeros((19, 1), dtype=object)
+    return messages
+
+def checkNeighborhood(imageP, i, j, w, h):
+    if j < w-1 and imageP[i, j+1] != 0:
+        return 1
+    if j < w-2 and imageP[i, j+2] != 0:
+        return 1
+    if i < h-1 and imageP[i+1, j] != 0:
+        return 1
+    if i < h-2 and imageP[i+2, j] != 0:
+        return 1
+    if j > 0 and imageP[i, j-1] != 0:
+        return 1
+    if j > 1 and imageP[i, j-2] != 0:
+        return 1
+    if i > 0 and imageP[i-1, j] != 0:
+        return 1
+    if i > 1 and imageP[i-2, j] != 0:
+        return 1
+    if i < h-1 and j < w-1 and imageP[i+1, j+1] != 0:
+        return 1
+    if i < h-1 and j > 0 and imageP[i+1, j-1] != 0:
+        return 1
+    if i > 0 and j < w-1 and imageP[i-1, j+1] != 0:
+        return 1
+    if i > 0 and j > 0 and imageP[i-1, j-1] != 0:
+        return 1
+    return 0
+
+def visualize_image(label_colors, features):
+    defaultG = []
+    imWidth = 640
+    imHeight = 360
     image = Image.new('RGB', (imWidth, imHeight))
     imagePix = image.load()
 
@@ -25,173 +232,50 @@ def main(w, h):
         for j in range(imWidth):
             imagePix[j, i] = (0, 0, 0)
 
-    mat = scipy.io.loadmat('loop1_casenet_features/loop1_casenet_features/VDO_0017_1298.mat')
-    features = mat['feat']
     vShape = features.shape
-    no_of_features = vShape[1]
     rows = vShape[0]
 
     # Pixel value with labels.
     for j in range(rows):
         row = features[j]
-        y = int(row[0] * imHeight)
-        x = int(row[1] * imWidth)
-        imagePix[x, y] = (255, 255, 255)
-        #for i in range(no_of_features):
-            #print(int(row[0]*imWidth), int(row[1]*imHeight))
-    image.show()
-    print('yes')
+        y = int(round(row[0] * imHeight))
+        x = int(round(row[1] * imWidth))
+        f_array = features[j]
+        f_array_np = np.array(f_array[2:])
+        idx = np.argmin(f_array_np)
+        val = label_colors[idx]
+        defaultG.append(idx)
+        imagePix[x, y] = val
+        # for i in range(no_of_features):
+        # print(int(row[0]*imWidth), int(row[1]*imHeight))
+    #image.show()
+    image.save('./Outputs/00.jpg')
+    defaultG = np.array(defaultG)
+    return defaultG
 
-def createGraphFromImage(G, w, h):
-    mat = scipy.io.loadmat('loop1_casenet_features/loop1_casenet_features/VDO_0017_1298.mat')
-    features = mat['feat']
+def preprocess_features(features):
     vShape = features.shape
     no_of_features = vShape[1]
     rows = vShape[0]
-
-    # Adding value nodes.
+    featuresL = np.empty((0,21), 'double')
     for j in range(rows):
-        row = features[j]
-        G.add_node(j, bipartite = 0)
+        f_vals = features[j]
+        probabilities = f_vals[2:]
+        m_val = np.argmax(probabilities)
+        if m_val >= 0.5:
+            featuresL = np.vstack((featuresL, f_vals))
 
-    # Calculating unary costs.
-    unary_cost = []
+    rows = featuresL.shape[0]
     for j in range(rows):
-        row = features[j]
-        cost = row[2:]
-        unary_cost.append(cost)
+        f_vals = featuresL[j]
+        probabilities = f_vals[2:]
+        probabilities = 1 - probabilities
+        probabilities = probabilities*10
+        f_vals[2:] = probabilities
+        featuresL[j] = f_vals
 
+    return featuresL
 
-    # #Adding factor nodes.
-    # messages = {}
-    # offset = rows
-    # count = 0
-    # for i in range(rows):
-    #     row = features[i]
-    #     x = row[0] * w
-    #     y = row[1] * h
-    #     pos = np.array([x,y])
-    #     for j in range(rows):
-    #         if i != j:
-    #             rowj = features[j]
-    #             xj = rowj[0] * w
-    #             yj = rowj[1] * h
-    #             posj = np.array([xj, yj])
-    #             d = np.linalg.norm(pos - posj)
-    #             if d < 2:
-    #                 #Create the factor node.
-    #                 #TODO: create dictionary to store the messages.
-    #                 id = offset + count
-    #                 G.add_node(id, bipartite=1)
-    #                 G.add_edge(id, i)
-    #                 G.add_edge(id, j)
-    #
-    #                 messages[str(id) + '-' + str(i)] = 0
-    #                 messages[str(id) + '-' + str(j)] = 0
-    #                 count += 1
-
-    # Adding factor nodes.
-    imageP = np.zeros((h, w), 'int')
-    messages = {}
-    for i in range(rows):
-        row = features[i]
-        y = int(row[0] * h)
-        x = int(row[1] * w)
-        imageP[y, x] = i
-
-    offset = rows
-    count = 0
-    for i in range(h):
-        for j in range(w):
-            if imageP[i, j] != 0:
-                remove_node = 1
-                #Node to the right.
-                if j < w-1 and imageP[i, j + 1] != 0:
-                    id = offset + count
-                    G.add_node(id, bipartite=1)
-                    G.add_edge(id, imageP[i, j])
-                    G.add_edge(id, imageP[i, j+1])
-                    count += 1
-                    createMessages(messages, str(id), str(imageP[i, j]))
-                    createMessages(messages, str(id), str(imageP[i, j + 1]))
-                    remove_node = 0
-
-                #Node to the bottom.
-                if i < h-1 and imageP[i + 1, j] != 0:
-                    id = offset + count
-                    G.add_node(id, bipartite=1)
-                    G.add_edge(id, imageP[i, j])
-                    G.add_edge(id, imageP[i + 1, j])
-                    count += 1
-                    createMessages(messages, str(id), str(imageP[i, j]))
-                    createMessages(messages, str(id), str(imageP[i + 1, j]))
-                    remove_node = 0
-
-                # Node to the diagnol right.
-                if i < h - 1 and j < w - 1 and imageP[i + 1, j + 1] != 0:
-                    id = offset + count
-                    G.add_node(id, bipartite=1)
-                    G.add_edge(id, imageP[i, j])
-                    G.add_edge(id, imageP[i + 1, j + 1])
-                    count += 1
-                    createMessages(messages, str(id), str(imageP[i, j]))
-                    createMessages(messages, str(id), str(imageP[i + 1, j + 1]))
-                    remove_node = 0
-
-                # Noe to the diagnol left.
-                if i < h - 1 and j > 0 and imageP[i + 1, j - 1] != 0:
-                    id = offset + count
-                    G.add_node(id, bipartite=1)
-                    G.add_edge(id, imageP[i, j])
-                    G.add_edge(id, imageP[i + 1, j - 1])
-                    count += 1
-                    createMessages(messages, str(id), str(imageP[i, j]))
-                    createMessages(messages, str(id), str(imageP[i + 1, j - 1]))
-                    remove_node = 0
-
-                #Node to the right 2 pixels.
-                if j < w-2 and imageP[i, j + 2] != 0:
-                    id = offset + count
-                    G.add_node(id, bipartite=1)
-                    G.add_edge(id, imageP[i, j])
-                    G.add_edge(id, imageP[i, j + 2])
-                    count += 1
-                    createMessages(messages, str(id), str(imageP[i, j]))
-                    createMessages(messages, str(id), str(imageP[i, j + 2]))
-                    remove_node = 0
-
-                #Node to the bottom 2 pixels.
-                if i < h-2 and imageP[i + 2, j] != 0:
-                    id = offset + count
-                    G.add_node(id, bipartite=1)
-                    G.add_edge(id, imageP[i, j])
-                    G.add_edge(id, imageP[i + 2, j])
-                    count += 1
-                    createMessages(messages, str(id), str(imageP[i, j]))
-                    createMessages(messages, str(id), str(imageP[i + 2, j]))
-                    remove_node = 0
-
-                #Removing nodes without any factor nodes.
-                if remove_node == 1:
-                    G.remove_node(imageP[i, j])
-
-
-
-    nx.write_gexf(G, './graph.gexf')
-    nbrs = G.neighbors(23629)
-    for nbr in nbrs:
-        print(nbr)
-        # nbrs1 = G.neighbors(nbr)
-        # for nbr1 in nbrs1:
-        #     print(nbr1)
-
-    beliefpropogation(G, unary_cost, rows, count, messages)
-    print('done!')
-
-def createMessages(messages, str1, str2):
-    messages[str1 + '-' + str2] = [0] * 19
-    messages[str2 + '-' + str1] = [0] * 19
-    return messages
 
 def createGraph(G, w, h):
     for i in range(h):
